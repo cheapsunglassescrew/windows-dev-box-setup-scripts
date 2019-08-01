adf9f1a444f102c74052590ad88c6ed4aaf5901a
cheapsunglassescrew <48054846+cheapsunglassescrew@users.noreply.github.com>
2019-08-01 22:27:34 +0200

    Update CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,28 @@
 
 # tools we expect devs across many scenarios will want
 choco install -y vscode
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y python
+choco install -y vscode-csharp
+choco install -y vscode-powershell
+# choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y github-desktop
+# choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
+choco install -y googlechrome
+choco install -y docker-desktop
+choco install -y vscode-docker
+choco install -y monogame
+choco install -y gimp
+choco install -y tiled
+choco install -y dotpeek
+choco install -y paint.net
+choco install -y poshgit
+
+choco install -y steam
+choco install -y goggalaxy
+choco install -y epicgameslauncher
+choco install -y origin
+choco install -y discord
+choco install -y icloud
+choco install -y whatsapp
+
