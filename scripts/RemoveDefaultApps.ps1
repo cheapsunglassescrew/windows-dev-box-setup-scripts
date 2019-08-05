31a765e7f6b67c81041f18771b1fa3d2f1f434f0
cheapsunglassescrew <48054846+cheapsunglassescrew@users.noreply.github.com>
2019-08-05 18:17:13 +0200

    Update RemoveDefaultApps.ps1

--- scripts/RemoveDefaultApps.ps1
+++ scripts/RemoveDefaultApps.ps1
@@ -8,9 +8,9 @@ Write-Host "Uninstall some applications that come with Windows out of the box" -
 # https://gist.github.com/alirobe/7f3b34ad89a159e6daa1
 # https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/remove-default-apps.ps1
 
-Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | where-object {$_.name –notlike "*photos*"}  | where-object {$_.name –notlike "*calc*"} | Write-Output
-# Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | where-object {$_.name –notlike "*photos*"}  | where-object {$_.name –notlike "*calc*"} | Remove-AppxPackage
+Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*photos*"}  | where-object {$_.name -notlike "*calc*"} | Write-Output
 
+# Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*photos*"}  | where-object {$_.name -notlike "*calc*"} | Remove-AppxPackage
 
 foreach ($app in $applicationList) {
     removeApp $app
